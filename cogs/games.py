64498f239b9dffb5c167b71909fcc1b147f959e5
Alex Davis <8m3xlt@gmail.com>
2020-08-16 23:19:28 +0100

    added minesweeper

--- cogs/games.py
+++ cogs/games.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import random
+import math
 
 rps_moves = ["🧱", "📰", "✂"]
 
@@ -92,6 +93,79 @@ class Games(commands.Cog):
                                                               f"ai played: {ai_move}\n{result}", inline=True)
 
             game_round += 1
+
+    @commands.command(aliases=["ms"])
+    async def minesweeper(self, ctx, x: int = 5, y: int = 5):
+        grid = [[0 for n in range(x)] for i in range(y)]  # Creates a grid using embeded lists
+        num_boms = math.ceil((x*y)//5)  # About 20% of the cells will be bombs
+
+        # Place the bombs
+        for n in range(num_boms):
+            while True:
+                bomb_x = random.randint(0, x - 1)
+                bomb_y = random.randint(0, y - 1)
+                bomb_space = grid[bomb_y][bomb_x]
+                if bomb_space == 0:
+                    # "*" represents a bomb
+                    grid[bomb_y][bomb_x] = "*"
+                    break
+                else:
+                    pass
+
+        # Update the empty spaces to show many bombs are adjacent
+        # The easiest thing to do is to find each bomb and add one to each adjacent cell
+        for row_num, row in enumerate(grid):
+            for column_num, item in enumerate(row):
+                # If the cell contains a bomb
+                if item == "*":
+
+                    # This loops over every adjacent cell
+                    for a in [-1, 0, 1]:
+                        for b in [-1, 0, 1]:
+                            try:
+                                coords = (row_num+a, column_num+b)
+                                target_item = grid[coords[0]][coords[1]]  # Get the contents of the cell
+
+                                # This cehcks that the cell doesn't contain a bomb, and that the coords aren't negative
+                                # Python does negative indexes which i forgot the first time i wrote this bit
+                                if target_item != "*" and coords[0] >= 0 and coords[1] >= 0:
+                                    grid[coords[0]][coords[1]] += 1
+                            except IndexError:
+                                # If the cell being looked at doesn't exist this runs
+                                # This is somewhat lazy but it works
+                                pass
+
+        # This is easier to do than finding the weird unicode characters like i did for the poll command
+        emoji_replacements = {
+            0: ":zero:",
+            1: ":one:",
+            2: ":two:",
+            3: ":three:",
+            4: ":four:",
+            5: ":five:",
+            6: ":six:",
+            7: ":seven:",
+            8: ":eight:",
+            9: ":nine:",
+            "*": ":boom:"
+        }
+
+        for row_num, row in enumerate(grid):
+            for column_num, item in enumerate(row):
+                # Replace everything in the list with the corresponding emoji markdown
+                grid[row_num][column_num] = emoji_replacements[item]
+
+        out_grid = []
+        for row in grid:
+            # Spoiler tag each cell
+            out_grid.append("".join([f"||{i}||" for i in row]))
+        out_string = "\n".join(out_grid) + f"\nThere are {num_boms} bombs in this grid!"
+
+        try:
+            await ctx.send(out_string)
+        except discord.HTTPException:
+            # This runs if the message is too long
+            await ctx.send("That grid is too big!")
 
 
 def setup(client):
